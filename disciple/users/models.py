e2e7cc4d72fbb2513b684e8ab84f7f515adf1bc4
Jonathan Hrovat <jmhrovat@gmail.com>
2021-01-25 19:20:25 -0800

    Update models.py

--- disciple/users/models.py
+++ disciple/users/models.py
@@ -8,3 +8,5 @@ class CustomUser(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.CharField(max_length=200)
+
+# Testing upload
