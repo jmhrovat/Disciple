e9099790ba1b1e2e73de8e9646e520c800ae39cd
Jonathan Hrovat <jmhrovat@gmail.com>
2021-01-27 20:08:16 -0800

    Create test_models.py

---
+++ disciple/users/tests/test_models.py
@@ -0,0 +1,25 @@
+from django.test import TestCase
+from users.models import Profile
+from django.contrib.auth.models import User
+
+
+class TestModels(TestCase):
+
+    def setUp(self):
+        self.test_user = User.objects.create(
+            first_name = "John",
+            last_name = "Doe",
+            username = "john_doe!",
+            password = "testpassword1234",
+            email = "jdoe@email.com"
+        )
+        self.test_user.save()
+
+
+    def test_profile_created_on_user_save(self):
+        try:
+            test_profile = Profile.objects.get(user=self.test_user)
+        except:
+            pass
+
+        self.assertEquals(test_profile.__str__(), "Doe, John")
