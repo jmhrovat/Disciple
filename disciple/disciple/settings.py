b28e8c002b2fb1f6e49910cc2bee8d6f75e8a2e7
Jonathan Hrovat <jmhrovat@gmail.com>
2021-02-23 10:18:30 -0800

    Reading page

--- disciple/disciple/settings.py
+++ disciple/disciple/settings.py
@@ -33,9 +33,9 @@ SECRET_KEY = secret_key
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.212']
+# ALLOWED_HOSTS = ['192.168.1.212']
 # Launch with 0.0.0.0:8000 for mobile testing
-# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = []
 
 
 
